d4c63ff002b6e9c8f64a76ce84272f34b5818c79
Admin <bskaya.mert@gmail.com>
2022-09-09 21:25:41 +0300

    Flash Messages and login page are added

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const expressSession = require('express-session')
 
 const app = express()
 
 const port = 3001
 const hostname = '127.0.0.1'
+
+app.use(expressSession({
+    secret: 'hiddenkeyhiddenkey',
+    resave: 'false',
+    saveUninitialized: true
+}))
 
 app.use(express.static('public'))
 
@@ -12,6 +20,16 @@ app.use(express.static('public'))
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
+// Body-parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+// Flash Message Middleware
+app.use((req, res, next) => {
+    res.locals.sessionFlash = req.session.sessionFlash
+    delete req.session.sessionFlash
+    next()
+})
 
 // Router
 const main = require('./router/main')
--- router/users.js
+++ router/users.js
@@ -5,4 +5,16 @@ router.get('/register', (req, res) => {
     res.render('site/register')
 })
 
+router.post('/register', (req, res) => {
+    req.session.sessionFlash = {
+        type:'alert alert-success',
+        message: 'You have registered successfully'
+    }
+    res.redirect('/users/login')
+})
+
+router.get('/login', (req, res) => {
+    res.render('site/login')
+})
+
 module.exports = router
