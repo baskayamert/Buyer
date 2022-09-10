9a5a65a1b0879f28fbb323176939681d9c68727a
Admin <bskaya.mert@gmail.com>
2022-09-10 15:07:33 +0300

    Db connection and registration oper. were added

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const expressSession = require('express-session')
+const connectMongo = require('connect-mongo')
+const mongoose = require('mongoose')
 
 const app = express()
 
 const port = 3001
 const hostname = '127.0.0.1'
 
+mongoose.connect('mongodb://127.0.0.1/ecommerce_db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
 app.use(expressSession({
     secret: 'hiddenkeyhiddenkey',
     resave: 'false',
-    saveUninitialized: true
+    saveUninitialized: true,
+    store: connectMongo.create({ mongoUrl: 'mongodb://127.0.0.1/ecommerce_db' })
 }))
 
 app.use(express.static('public'))
@@ -23,6 +31,21 @@ app.set('view engine', 'handlebars')
 // Body-parser
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+
+// The middleware which determines what is shown to users
+app.use((req, res, next) => {
+    const { userId } = req.session
+    if(userId) {
+        res.locals = {
+            displayLink: true
+        }
+    } else {
+        res.locals = {
+            displayLink: false
+        }
+    }
+    next()
+})
 
 // Flash Message Middleware
 app.use((req, res, next) => {
--- router/users.js
+++ router/users.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const User = require('../models/User')
 const router = express.Router()
 
 router.get('/register', (req, res) => {
@@ -6,15 +7,40 @@ router.get('/register', (req, res) => {
 })
 
 router.post('/register', (req, res) => {
-    req.session.sessionFlash = {
-        type:'alert alert-success',
-        message: 'You have registered successfully'
-    }
-    res.redirect('/users/login')
+    User.create(req.body, (error, user) => {
+        req.session.sessionFlash = {
+            type:'alert alert-success',
+            message: 'You have registered successfully'
+        }
+        res.redirect('/users/login')
+    })
 })
 
 router.get('/login', (req, res) => {
     res.render('site/login')
+})
+
+router.post('/login', (req, res) => {
+    const {email, password} = req.body
+    User.findOne({email}, (error, user) => {
+        if(user){
+            if(user.password === password) {
+                //USER SESSION
+                req.session.userId = user._id
+                res.redirect('/')
+            } else {
+                res.redirect('/users/login')
+            }
+        } else {
+            res.redirect('/users/register')
+        }
+    })
+})
+
+router.get('/logout', (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/')
+    })
 })
 
 module.exports = router
