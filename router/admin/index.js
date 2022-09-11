22fc786576adeefe90d25b510ccb1f639e24d84f
Admin <bskaya.mert@gmail.com>
2022-09-12 01:49:27 +0300

    Navbar and category were added for admins

--- router/admin/index.js
+++ router/admin/index.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const User = require('../../models/User')
+const Category = require('../../models/Category')
 const router = express.Router()
 
 router.get('/', (req, res) => {
+    res.render('admin/index')
+})
+
+//Users
+
+router.get('/users', (req, res) => {
     User.find({}).sort({ $natural: -1 }).lean().then(users => {
-        res.render('admin/index', { users: users})
+        res.render('admin/users', { users: users})
     })
 })
 
-router.delete('/:id', (req, res) => {
+router.delete('/users/:id', (req, res) => {
     User.deleteOne({ _id: req.params.id }).lean().then(users => {
-        res.redirect('/admin')
+        res.redirect('/admin/users')
     })
 })
 
@@ -26,7 +33,33 @@ router.put('/users/edit/:id', (req, res) => {
         email: req.body.email,
         password: req.body.password
     }}).lean().then(user => {
-        res.redirect('/admin')
+        res.redirect('/admin/users')
+    })
+})
+
+//Categories
+
+router.get('/categories', (req, res) => {
+    Category.find({}).sort({ $natural: -1 }).lean().then(categories => {
+        res.render('admin/categories', {categories:categories})
+    })
+})
+
+router.post('/categories', (req, res) => {
+    Category.create(req.body, (error, category) => {
+        if(!error){
+            req.session.sessionFlash = {
+                type:'alert alert-success',
+                message: 'The category has been successfully added!'
+            }
+            res.redirect('/admin/categories')
+        }
+    })
+})
+
+router.delete('/categories/:id', (req, res) => {
+    Category.deleteOne({ _id: req.params.id }).lean().then(categories => {
+        res.redirect('/admin/categories')
     })
 })
 
