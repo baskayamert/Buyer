8f0bee933c381bad5bb032b88e9a810b927bdcef
Admin <bskaya.mert@gmail.com>
2022-09-17 23:14:20 +0300

    Products can be seen in the homepage now.

--- router/main.js
+++ router/main.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const Category = require('../models/Category')
+const Product = require('../models/Product')
 const router = express.Router()
 
 router.get('/', (req, res) => {
     Category.find({}).sort({name: 1}).lean().then(categories => {
-        res.render('site/index', {categories: categories})
+        Product.find({}).lean().then(products => {
+            res.render('site/index', {categories: categories, products: products})
+        })
+        
     })
     
 })
