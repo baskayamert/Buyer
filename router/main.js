d8a3dbc517ee9585a86559d11fbf0635bf64bbe4
Admin <bskaya.mert@gmail.com>
2022-09-18 01:11:50 +0300

    The category filter and new products were added.

--- router/main.js
+++ router/main.js
@@ -13,4 +13,12 @@ router.get('/', (req, res) => {
     
 })
 
+router.get('/category/:categoryId', (req, res) => {
+    Product.find({category:req.params.categoryId}).populate({path:'category', model:Category}).lean().then(products => {
+        Category.find({}).sort({name: 1}).lean().then(categories => {
+            res.render('site/index', {categories: categories, products: products})
+        })
+    })
+})
+
 module.exports = router
